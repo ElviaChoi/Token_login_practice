48560a295dcf79d10214a892c404ee6d354ee440
ElviaChoi <elviachoi924@gmail.com>
2025-06-27 16:04:44 +0900

    node.js 4일차 과제

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
-    ],
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
   })
@@ -42,8 +39,20 @@ app.post("/", (req, res) => {
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
     // 이곳에 코드를 작성하세요.
+    const accessToken = jwt.sign(
+      {
+        userId: userInfo.user_id,
+        user_name: userInfo.user_name,
+        user_info: userInfo.user_info,
+      },
+      secretKey,
+      {
+        expiresIn: "1h",
+      }
+    );
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
     // 이곳에 코드를 작성하세요.
+    res.send(accessToken);
   }
 });
 
@@ -51,8 +60,26 @@ app.post("/", (req, res) => {
 app.get("/", (req, res) => {
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
   // 이곳에 코드를 작성하세요.
-  // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
-  // 이곳에 코드를 작성하세요.
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send("토큰이 제공되지 않습니다.");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
+    // 이곳에 코드를 작성하세요.
+    res.send({
+      user_id: decoded.user_id,
+      user_name: decoded.user_name,
+      user_info: decoded.user_info,
+    });
+  } catch (error) {
+    res.status(401).send("유효하지 않은 토큰입니다.");
+  }
 });
 
 app.listen(3000, () => console.log("서버 실행!"));
